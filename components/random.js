5de32c424c200bf7716b758d36cdb623bba86ca4
orels1 <orel-s1@yandex.ru>
2018-11-11 04:19:11 +0300

    random implementation

--- components/random.js
+++ components/random.js
@@ -1,4 +1,7 @@
 components[`${prefix}_load_random`] = () => {
+  const name = 'random';
+  const lprefix = `${prefix}_${name}`;
+
   const interval = setInterval(() => {
     if (!Vue) {
       return;
@@ -6,12 +9,33 @@ components[`${prefix}_load_random`] = () => {
     if (Vue) {
       coolLog('Injecting RANDOM components');
       new Vue({
-        el: `#${prefix}_random`,
+        el: `#${lprefix}`,
         template: `
-          <div id="${prefix}_random">
-            ...or pick a random one 🎲
+          <div id="${lprefix}" @click="loadRandom">
+            <div class="${lprefix}_text">...or pick a random one</div>
+            <div class="${lprefix}_dice">🎲</div>
           </div>
-        `
+        `,
+        data: () => ({
+          total: 0
+        }),
+        async mounted() {
+          const resp = await fetch('https://api.rawg.io/api/games?limit=1');
+          if (!resp.ok) {
+            return;
+          }
+          const json = await resp.json();
+          this.total = json.count;
+        },
+        methods: {
+          loadRandom() {
+            let index = Math.floor(Math.random() * this.total);
+            if (index === 0) {
+              index = 1;
+            }
+            window.location.assign(`https://rawg.io/games/${index}`);
+          }
+        }
       });
 
       clearInterval(interval);
